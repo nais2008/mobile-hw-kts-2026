73bc97b3e52c35179c34a585bfa341ab297f017f
nais2008 <t09n09a08@gmail.com>
2026-02-24 15:53:04 +0500

    to repo

--- composeApp/src/commonMain/kotlin/com/github/nais2008/mobile_hw_kts_2026/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.github.nais2008.mobile_hw_kts_2026
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/iosMain/kotlin/com/github/nais2008/mobile_hw_kts_2026/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.github.nais2008.mobile_hw_kts_2026
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
